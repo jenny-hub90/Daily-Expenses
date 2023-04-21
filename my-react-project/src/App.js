04c81f074c95636a9ae240c7fe8498df6f77cccc
jenny-hub90 <jumbledjenny19@gmail.com>
2023-04-21 16:58:40 +0545

    made the list of expenses dynamic

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import NewExpenses from "./components/Expenses/NewExpenses";
-
-const App =() => {
-  const expenses = [
-    { 
-      id: 'e1',
-      title : 'Toilet Paper',
-      amount : 345,
-      date: new Date(2023, 4, 13),
-    },
-    { 
-      id: 'e2',
-      title : 'Scooter Insurance',
-      amount : 5000,
-      date: new Date(2023, 4, 13),
-    },
-    { 
-      id: 'e3',
-      title : 'Tumbler',
-      amount : 2800,
-      date: new Date(2023, 4, 13),
-    },
-    { 
-      id: 'e4',
-      title : 'Car Insurance',
-      amount : 20000,
-      date: new Date(2023, 4, 13),
-    },
-  ];
-
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+const DUMMY_EXPENSES = [
+  { 
+    id: 'e1',
+    title : 'Toilet Paper',
+    amount : 345,
+    date: new Date(2023, 4, 13),
+  },
+  { 
+    id: 'e2',
+    title : 'Scooter Insurance',
+    amount : 5000,
+    date: new Date(2023, 4, 13),
+  },
+  { 
+    id: 'e3',
+    title : 'Tumbler',
+    amount : 2800,
+    date: new Date(2023, 4, 13),
+  },
+  { 
+    id: 'e4',
+    title : 'Car Insurance',
+    amount : 20000,
+    date: new Date(2023, 4, 13),
+  },
+];
+const App = () => {
+    const [expenses, setExpenses]=useState(DUMMY_EXPENSES);
+    const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
 
